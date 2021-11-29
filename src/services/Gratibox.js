72f0913bf421ad146109274496c4ca8391d36c5d
victorMayer <victor.mayer@outlook.com>
2021-11-29 12:17:31 -0300

    added url to deployed backend

--- src/services/Gratibox.js
+++ src/services/Gratibox.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? /*put deploy link*/"" : "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? "https://gratibox.herokuapp.com" : "http://localhost:4000";
 
 function createConfig(token) {
     const config = {
